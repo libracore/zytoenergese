c47ad0363c81a54cfb34b9b5fcc51d5e622b1884
Ivanios <ivan.lochbihler@libracore.com>
2024-01-11 20:48:11 +0100

    report device

--- zytoenergese/zytoenergese/report/device_overview/device_overview.py
+++ zytoenergese/zytoenergese/report/device_overview/device_overview.py
@@ -2,8 +2,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 
 def execute(filters=None):
-	columns, data = [], []
+	columns = get_columns()
+	data = get_data()
 	return columns, data
+	
+def get_columns():
+	columns = [
+		{"label": _("Device type"), "fieldname": "device_type", "fieldtype": "Data", "width": 100},
+		{"label": _("Device Name"), "fieldname": "device_name", "fieldtype": "Data", "width": 100},
+		{"label": _("Sales Order"), "fieldname": "to_order", "fieldtype": "Data", "width": 100},
+		{"label": _("Customer"), "fieldname": "customer", "fieldtype": "Data", "width": 100},
+		{"label": _("Transaction Type"), "fieldname": "transaction_type", "fieldtype": "Data", "width": 100},
